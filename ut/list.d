79fb8f4b47b0afadd19f623992495cc34fb8e6e7
Atila Neves <aalvesne@cisco.com>
2013-07-08 18:36:49 +0200

    Added tests to list.d

--- ut/list.d
+++ ut/list.d
@@ -13,3 +13,34 @@ string[] getTestClassNames(alias mod)() {
 
     return classes;
 }
+
+string[] getTestFunctions(alias mod)() {
+    mixin("import " ~ fullyQualifiedName!mod ~ ";"); //so it's visible
+    string[] functions = [];
+    foreach(moduleMember; __traits(allMembers, mod)) {
+        static immutable prefix = "test";
+        static if(moduleMember.length >= prefix.length && moduleMember[0 .. prefix.length] == "test") {
+            functions ~= fullyQualifiedName!mod ~ "." ~ moduleMember;
+        }
+    }
+
+    return functions;
+}
+
+string[] getTestables(alias mod)() {
+    return getTestFunctions!mod ~ getTestClassNames!mod;
+}
+
+private void testFoo() {}
+private void testBar() {}
+private void someFun() {}
+private class FooTest { void test() { } }
+private class Bar { }
+
+unittest {
+    import std.conv;
+    const actualFuncs = getTestFunctions!(mixin(__MODULE__))();
+    const expectedFuncs = [ "ut.list.testFoo", "ut.list.testBar" ];
+    assert(actualFuncs == expectedFuncs, "Expected " ~ to!string(expectedFuncs) ~
+           ", got: " ~ to!string(actualFuncs));
+}
