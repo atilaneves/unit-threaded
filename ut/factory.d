2671e6c1967dced5b6f3717491e8adaffe1fa07f
Atila Neves <aalvesne@cisco.com>
2013-07-11 11:01:43 +0200

    Refactored factory.d

--- ut/factory.d
+++ ut/factory.d
@@ -5,23 +5,26 @@ import ut.list;
 import std.stdio;
 import std.traits;
 import std.typetuple;
+import std.exception;
 
 /**
  * Creates tests cases from the given modules
  */
 TestCase[] createTests(MODULES...)() {
-    string[] testCaseNames;
-    foreach(mod; TypeTuple!MODULES) {
-        testCaseNames ~= getTestClassNames!mod();
-    }
-
     TestCase[] tests;
-    foreach(name; testCaseNames) {
+    foreach(name; getAllTests!(q{getTestClassNames}, MODULES)()) {
         auto test = cast(TestCase) Object.factory(name);
         assert(test !is null, "Could not create object of type " ~ name);
         tests ~= test;
-
     }
 
     return tests;
+}
+
+private auto getAllTests(string expr, MODULES...)() {
+    string[] functions;
+    foreach(mod; TypeTuple!MODULES) {
+        functions ~= mixin(expr ~ q{!mod()});
+    }
+    return assumeUnique(functions);
 }
