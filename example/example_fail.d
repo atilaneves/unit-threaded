196d85986442f41b1ffc028ddd5102522b91109c
Atila Neves <aalvesne@cisco.com>
2013-07-17 14:30:39 +0200

    Cleaned up example_fail.d

--- example/example_fail.d
+++ example/example_fail.d
@@ -1,12 +1,12 @@
 #!/usr/bin/rdmd -unittest
 
 import ut.runner;
-
 import std.stdio;
 
 
 int main() {
     writeln("Running failing unit-threaded examples...\n");
+    //fail_tests and pass_tests are two modules in this directory
     immutable success = runTests!("fail_tests", "pass_tests")();
     return success ? 0 : 1;
 }
