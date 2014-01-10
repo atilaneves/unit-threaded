df299718486b2a0b2202a44bf7fd89442bcb25c2
Atila Neves <aalvesne@cisco.com>
2014-01-10 19:28:57 +0100

    Fixed/removed comment in example_pass.d

--- example/example_pass.d
+++ example/example_pass.d
@@ -12,8 +12,6 @@ import std.stdio;
 
 int main(string[] args) {
     writeln("Unit-threaded examples: Passing\n");
-    ///pass_tests is a modules in the tests directory
-    ///no import necessary at the top, passed in as strings
     return runTests!(tests.pass.normal,
                      tests.pass.delayed,
                      tests.pass.attributes,
