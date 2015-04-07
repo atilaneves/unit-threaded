c3bb20192698a8c1713fe2483024da87c61887a1
Atila Neves <aalvesne@cisco.com>
2015-04-07 20:56:42 +0000

    TestCase refactoring and removal of class-based test functionality

--- source/unit_threaded/testcase.d
+++ source/unit_threaded/testcase.d
@@ -5,15 +5,8 @@ import unit_threaded.io;
 import unit_threaded.reflection: TestData, TestFunction;
 
 import std.exception;
-import std.string;
-import std.conv;
 import std.algorithm;
 
-
-struct TestResult {
-    int failures;
-    string output;
-}
 
 /**
  * Class from which other test cases derive
@@ -35,9 +28,13 @@ class TestCase {
 
     final auto collectOutput() {
         print(getPath() ~ ":\n");
-        check(setup());
-        check(test());
-        check(shutdown());
+        try {
+            test();
+        } catch(UnitTestException ex) {
+            fail(ex.msg);
+        } catch(Exception ex) {
+            fail("\n    " ~ ex.toString() ~ "\n");
+        }
         if(_failed) print("\n\n");
     }
 
@@ -45,26 +42,12 @@ class TestCase {
         utWrite(_output);
     }
 
-    void setup() { } ///override to run before test()
-    void shutdown() { } ///override to run after test()
     abstract void test();
     ulong numTestsRun() const { return 1; }
 
 private:
     bool _failed;
     string _output;
-
-    bool check(E)(lazy E expression) {
-        try {
-            expression();
-        } catch(UnitTestException ex) {
-            fail(ex.msg);
-        } catch(Exception ex) {
-            fail("\n    " ~ ex.toString() ~ "\n");
-        }
-
-        return !_failed;
-    }
 
     void fail(in string msg) {
         _failed = true;
@@ -76,49 +59,6 @@ private:
     }
 }
 
-class CompositeTestCase: TestCase {
-    void add(TestCase t) { _tests ~= t;}
-
-    void opOpAssign(string op : "~")(TestCase t) {
-        add(t);
-    }
-
-    override string[] opCall() {
-        return _tests.map!(a => a()).reduce!((a, b) => a ~ b);
-    }
-
-    override void test() { assert(false, "CompositeTestCase.test should never be called"); }
-
-    override ulong numTestsRun() const {
-        return _tests.length;
-    }
-
-private:
-
-    TestCase[] _tests;
-}
-
-
-class ShouldFailTestCase: TestCase {
-    this(TestCase testCase) {
-        this.testCase = testCase;
-    }
-
-    override string getPath() const pure nothrow {
-        return this.testCase.getPath;
-    }
-
-    override void test() {
-        const ex = collectException!Exception(testCase.test());
-        if(ex is null) {
-            throw new Exception("Test " ~ testCase.getPath() ~ " was expected to fail but did not");
-        }
-    }
-
-private:
-
-    TestCase testCase;
-}
 
 class FunctionTestCase: TestCase {
     this(immutable TestData data) pure nothrow {
@@ -143,4 +83,49 @@ class FunctionTestCase: TestCase {
 
     private string _name;
     private TestFunction _func;
+}
+
+
+class ShouldFailTestCase: TestCase {
+    this(TestCase testCase) {
+        this.testCase = testCase;
+    }
+
+    override string getPath() const pure nothrow {
+        return this.testCase.getPath;
+    }
+
+    override void test() {
+        const ex = collectException!Exception(testCase.test());
+        if(ex is null) {
+            throw new Exception("Test " ~ testCase.getPath() ~ " was expected to fail but did not");
+        }
+    }
+
+private:
+
+    TestCase testCase;
+}
+
+
+class CompositeTestCase: TestCase {
+    void add(TestCase t) { _tests ~= t;}
+
+    void opOpAssign(string op : "~")(TestCase t) {
+        add(t);
+    }
+
+    override string[] opCall() {
+        return _tests.map!(a => a()).reduce!((a, b) => a ~ b);
+    }
+
+    override void test() { assert(false, "CompositeTestCase.test should never be called"); }
+
+    override ulong numTestsRun() const {
+        return _tests.length;
+    }
+
+private:
+
+    TestCase[] _tests;
 }
