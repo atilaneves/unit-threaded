3a14ce1a903437f788a1b4cf38beb863a8996a78
Atila Neves <aalvesne@cisco.com>
2015-04-12 13:46:22 +0000

    More explicit imports in runner.d

--- source/unit_threaded/runner.d
+++ source/unit_threaded/runner.d
@@ -1,9 +1,9 @@
 module unit_threaded.runner;
 
 import unit_threaded.testsuite;
-import unit_threaded.io;
 import unit_threaded.options;
-import unit_threaded.testcase;
+import unit_threaded.io: enableDebugOutput, forceEscCodes;
+import unit_threaded.testcase: TestData;
 import unit_threaded.reflection: allTestData;
 
 import std.conv: text;
