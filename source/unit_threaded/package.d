846a663db2b5ae9e85a098cecd000ad91cd95f4a
Atila Neves <aalvesne@cisco.com>
2015-10-20 14:34:58 +0200

    runner is now publicly imported

--- source/unit_threaded/package.d
+++ source/unit_threaded/package.d
@@ -5,3 +5,4 @@ public import unit_threaded.testcase;
 public import unit_threaded.io;
 public import unit_threaded.should;
 public import unit_threaded.reflection;
+public import unit_threaded.runner;
