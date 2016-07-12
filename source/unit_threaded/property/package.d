6dec9b5d8c1903ba9530009bf0f571e66a365051
Atila Neves <atila.neves@gmail.com>
2016-07-12 17:46:50 +0200

    shrink unsigneds

--- source/unit_threaded/property/package.d
+++ source/unit_threaded/property/package.d
@@ -5,6 +5,9 @@ public import unit_threaded.should;
 import unit_threaded.randomized.gen;
 import unit_threaded.randomized.random;
 import std.random: Random, unpredictableSeed;
+import std.traits: isIntegral;
+
+version(unittest) import unit_threaded.asserts;
 
 Random gRandom;
 
@@ -21,16 +24,13 @@ void check(alias F)(int numFuncCalls = 100,
     static assert(is(ReturnType!F == bool),
                   text("check only accepts functions that return bool, not ", ReturnType!F.stringof));
 
-    void error(T...)(T input) {
-        throw new UnitTestException(["Property failed with input:", input.to!string], file, line);
-    }
-
     auto gen = RndValueGen!(Parameters!F)(&gRandom);
+
     import unit_threaded.io;
     foreach(i; 0 .. numFuncCalls) {
         gen.genValues;
         if(!F(gen.values))
-            error(gen.values);
+            throw new UnitTestException(["Property failed with input:", gen.values.to!string], file, line);
     }
 }
 
@@ -64,8 +64,6 @@ void check(alias F)(int numFuncCalls = 100,
 
 @("Verify property that sometimes succeeds")
 @safe unittest {
-    import unit_threaded.asserts;
-
     // 2^100 is ~1.26E30, so the chances that no even length array is generated
     // is small enough to disconsider even if it were truly random
     // since Gen!int[] is front-loaded, it'll fail on the second attempt
@@ -79,4 +77,84 @@ void check(alias F)(int numFuncCalls = 100,
 @safe unittest {
     check!((Gen!(int, 1, 1) a) => a == 1);
     check!((Gen!(int, 1, 1) a) => a == 2).shouldThrow!UnitTestException;
+}
+
+
+T shrink(alias F, T)(T value) {
+    return shrinkImpl!F(value, [value]);
+}
+
+T shrinkImpl(alias F, T)(T value, T[] values) if(isIntegral!T) {
+    import std.conv: text;
+    import std.algorithm: canFind, minPos, maxPos;
+    import std.traits: isSigned;
+
+    assert(!F(value), text("Property did not fail for value ", value));
+
+    // import std.stdio;
+    // writeln("value: ", value, ", values: ", values);
+
+    if(F(value + 1)) return value;
+    if(F(value - 1)) return value;
+
+    bool try_(T attempt) {
+        if(!F(attempt) && !values.canFind(attempt)) {
+            values ~= attempt;
+            return true;
+        }
+
+        return false;
+    }
+
+    T[] attempts;
+    static if(isSigned!T) attempts ~= -value;
+    attempts ~= value / 2;
+    if(value < T.max / 2) attempts ~= cast(T)(value * 2);
+    if(value < T.max) attempts ~= cast(T)(value + 1);
+    if(value > T.min) attempts ~= cast(T)(value - 1);
+
+    foreach(attempt; attempts)
+        if(try_(attempt))
+            return shrinkImpl!F(attempt, values);
+
+    auto min = values.minPos[0];
+    auto max = values.maxPos[0];
+
+    if(!F(min)) return shrinkImpl!F(min, values);
+    if(!F(max)) return shrinkImpl!F(max, values);
+
+    return values[0];
+}
+
+
+
+@("shrink int when already shrunk")
+@safe unittest {
+    assertEqual(0.shrink!(a => a != 0), 0);
+}
+
+
+@("shrink int when not already shrunk going up")
+@safe unittest {
+    assertEqual(0.shrink!(a => a > 3), 3);
+}
+
+@("shrink int when not already shrunk going down")
+@safe unittest {
+    assertEqual(10.shrink!(a => a < -3), -3);
+}
+
+@("shrink int.max")
+@safe unittest {
+    assertEqual(int.max.shrink!(a => a == 0), 1);
+    assertEqual(int.min.shrink!(a => a == 0), -1);
+}
+
+@("shrink unsigneds")
+@safe unittest {
+    import std.meta;
+    foreach(T; AliasSeq!(ubyte, ushort, uint, ulong)) {
+        T value = 3;
+        assertEqual(value.shrink!(a => a == 0), 1);
+    }
 }
