7ea3a078d5f0c057ecb0aa41a41e2dc58e8d51e1
Atila Neves <atila.neves@gmail.com>
2016-07-14 16:44:36 +0200

    Can expect the parameters passed to the mocked function

--- source/unit_threaded/mock.d
+++ source/unit_threaded/mock.d
@@ -1,6 +1,8 @@
 module unit_threaded.mock;
 
+import unit_threaded.should: fail;
 import std.traits;
+import std.typecons;
 
 version(unittest) {
     import unit_threaded.asserts;
@@ -21,6 +23,7 @@ string implMixinStr(T)() {
 
         static if(__traits(isAbstractFunction, member)) {
 
+            enum parameters = Parameters!member.stringof;
             enum returnType = ReturnType!member.stringof;
 
             static if(is(ReturnType!member == void))
@@ -28,41 +31,83 @@ string implMixinStr(T)() {
             else
                 enum returnDefault = `    return ` ~ returnType ~ ".init;";
 
-            lines ~= `override ` ~ returnType ~ " " ~ m ~ Parameters!member.stringof ~ " {";
-            lines ~= `   called = true;`;
+            lines ~= `override ` ~ returnType ~ " " ~ m ~ typeAndArgsParens!(Parameters!member) ~ ` {`;
+            lines ~= `    called = true;`;
+            lines ~= `    ` ~ m ~ `_called = tuple` ~ argNamesParens(arity!member) ~ `;`;
             lines ~= returnDefault;
             lines ~= `}`;
+            lines ~= "";
+            lines ~= `Tuple!` ~ parameters ~ ` ` ~ m ~ `_called;`;
+            lines ~= "";
         }
     }
 
     return lines.join("\n");
 }
 
+private string argNamesParens(int N) @safe pure {
+    return "(" ~ argNames(N) ~ ")";
+}
+
+private string argNames(int N) @safe pure {
+    import std.range;
+    import std.algorithm;
+    import std.conv;
+    return iota(N).map!(a => "arg" ~ a.to!string).join(", ");
+}
+
+private string typeAndArgsParens(T...)() {
+    import std.array;
+    import std.conv;
+    string[] parts;
+    foreach(i, t; T)
+        parts ~= T[i].stringof ~ " arg" ~ i.to!string;
+    return "(" ~ parts.join(", ") ~ ")";
+}
+
 
 struct Mock(T) {
 
     private bool verified;
+    private string expected;
     auto _mocked = new MockAbstract;
 
     alias _mocked this;
 
     class MockAbstract: T {
         bool called;
+        alias checkerFunc = string delegate() pure @safe;
+        checkerFunc[] checkers;
+
         //pragma(msg, implMixinStr!T);
         mixin(implMixinStr!T);
+
+        void expectedValues(string func, V...)(V values) {
+            import std.conv: to;
+            mixin(`checkers ~= () { return tuple(values) == ` ~ func ~ `_called ? "" : ` ~ func ~ `_called.to!string;};`);
+        }
     }
 
-    ~this() {
+    ~this() pure @safe {
         if(!verified) verify;
     }
 
-    void expect(U...)(U) {
+    void expect(string func, V...)(V values) {
+        expected = func;
+        static if(V.length > 0) {
+            _mocked.expectedValues!func(values);
+        }
     }
 
-    void verify(string file = __FILE__, ulong line = __LINE__) {
+    void verify(string file = __FILE__, ulong line = __LINE__) @safe pure {
         import unit_threaded.should: fail;
         verified = true;
-        if(!called) fail("Expected call did not happen", file, line);
+        if(!called) fail("Expected call to " ~ expected ~ " did not happen", file, line);
+        foreach(checker; _mocked.checkers) {
+            auto res = checker();
+            if(res != "")
+                fail(expected ~ " was called with wrong parameters " ~ res, file, line);
+        }
     }
 }
 
@@ -70,7 +115,7 @@ auto mock(T)() {
     return Mock!T();
 }
 
-@("mock interface positive test")
+@("mock interface positive test no params")
 @safe pure unittest {
     interface Foo {
         int foo(int, string) @safe pure;
@@ -82,9 +127,42 @@ auto mock(T)() {
     }
 
     auto m = mock!Foo;
-    m.expect(&m.foo);
+    m.expect!"foo";
     fun(m);
 }
+
+@("mock interface positive test with params")
+@safe pure unittest {
+    interface Foo {
+        int foo(int, string) @safe pure;
+        void bar() @safe pure;
+    }
+
+    int fun(Foo f) {
+        return 2 * f.foo(5, "foobar");
+    }
+
+    {
+        auto m = mock!Foo;
+        m.expect!"foo"(5, "foobar");
+        fun(m);
+    }
+
+    {
+        auto m = mock!Foo;
+        m.expect!"foo"(6, "foobar");
+        fun(m);
+        m.verify.shouldThrowWithMessage(`foo was called with wrong parameters Tuple!(int, string)(5, "foobar")`);
+    }
+
+    {
+        auto m = mock!Foo;
+        m.expect!"foo"(5, "quux");
+        fun(m);
+        m.verify.shouldThrowWithMessage(`foo was called with wrong parameters Tuple!(int, string)(5, "foobar")`);
+    }
+}
+
 
 @("mock interface negative test")
 @safe pure unittest {
@@ -93,8 +171,8 @@ auto mock(T)() {
     }
 
     auto m = mock!Foo;
-    m.expect(&m.foo);
-    m.verify.shouldThrowWithMessage("Expected call did not happen");
+    m.expect!"foo";
+    m.verify.shouldThrowWithMessage("Expected call to foo did not happen");
 }
 
 // can't be in the unit test itself
@@ -113,6 +191,6 @@ private class Class {
     }
 
     auto m = mock!Class;
-    m.expect(&m.foo);
+    m.expect!"foo";
     fun(m);
 }
