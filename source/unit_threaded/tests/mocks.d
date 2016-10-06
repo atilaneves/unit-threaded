3b31f98ba884a5385a6cd4a71d2e0e7ea2307c0a
Atila Neves <atila.neves@gmail.com>
2016-10-06 22:02:01 +0200

    Uncomment out test

--- source/unit_threaded/tests/mocks.d
+++ source/unit_threaded/tests/mocks.d
@@ -1,10 +1,10 @@
 import unit_threaded;
 
-// @("private or protected members") @safe pure unittest {
-//     interface InterfaceWithProtected {
-//         bool result();
-//         protected final void inner(int i) { }
-//     }
+@("private or protected members") @safe pure unittest {
+    interface InterfaceWithProtected {
+        bool result();
+        protected final void inner(int i) { }
+    }
 
-//     auto m = mock!InterfaceWithProtected;
-// }
+    auto m = mock!InterfaceWithProtected;
+}
