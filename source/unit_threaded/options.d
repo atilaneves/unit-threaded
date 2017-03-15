449ac9bf1f693a0fe78f3c6096702834d36982a3
Atila Neves <aalvesne@cisco.com>
2017-03-15 19:46:33 -0300

    version unitUnthreaded with no threads anywhere

--- source/unit_threaded/options.d
+++ source/unit_threaded/options.d
@@ -66,6 +66,9 @@ auto getOptions(string[] args) {
         single = true;
     }
 
+    version(unitUnthreaded)
+        single = true;
+
     immutable exit =  help || list;
     return Options(!single, args[1..$], debugOutput, list, exit, forceEscCodes,
                    random, seed, stackTraces, showChrono);
