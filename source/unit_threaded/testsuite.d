841a0f9cdd969ef947d1b28f62a6e994e9f3cbf6
Atila Neves <atila.neves@gmail.com>
2018-04-04 15:19:42 +0200

    Refactoring

--- source/unit_threaded/testsuite.d
+++ source/unit_threaded/testsuite.d
@@ -215,6 +215,7 @@ version(unitThreadedLight) {
 
     shared static this() {
         import std.algorithm: canFind;
+        import std.parallelism: parallel;
         import core.runtime: Runtime;
 
         Runtime.moduleUnitTester = () {
@@ -228,19 +229,16 @@ version(unitThreadedLight) {
             }
 
             version(unitUnthreaded)
+                enum singleThreaded = true;
+            else
+                const singleThreaded = Runtime.args.canFind("-s") || Runtime.args.canFind("--single");
+
+            if(singleThreaded)
                 foreach(module_; modules)
                     module_.unitTest()();
-            else {
-                import std.parallelism: parallel;
-                if(Runtime.args.canFind("-s") || Runtime.args.canFind("--single")) {
-                    foreach(module_; modules)
-                        module_.unitTest()();
-                } else {
-                    foreach(module_; modules.parallel) {
-                        module_.unitTest()();
-                    }
-                }
-            }
+             else
+                foreach(module_; modules.parallel)
+                    module_.unitTest()();
 
             return true;
         };
