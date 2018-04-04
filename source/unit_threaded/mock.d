3f6f851e191d2de935834a5e6df12bbad53a6c98
Atila Neves <atila.neves@gmail.com>
2018-04-04 09:50:25 +0200

    mock

--- source/unit_threaded/mock.d
+++ source/unit_threaded/mock.d
@@ -288,6 +288,8 @@ auto mock(T)() {
     fun(m);
 }
 
+
+///
 @("mock interface positive test with params")
 @safe pure unittest {
     import unit_threaded.asserts;
@@ -301,87 +303,11 @@ auto mock(T)() {
         return 2 * f.foo(5, "foobar");
     }
 
-    {
-        auto m = mock!Foo;
-        m.expect!"foo"(5, "foobar");
-        fun(m);
-    }
-
-    {
-        auto m = mock!Foo;
-        m.expect!"foo"(6, "foobar");
-        fun(m);
-        assertExceptionMsg(m.verify,
-                           `    source/unit_threaded/mock.d:123 - foo was called with unexpected Tuple!(int, string)(5, "foobar")` ~ "\n" ~
-                           `    source/unit_threaded/mock.d:123 -        instead of the expected Tuple!(int, string)(6, "foobar")`);
-    }
-
-    {
-        auto m = mock!Foo;
-        m.expect!"foo"(5, "quux");
-        fun(m);
-        assertExceptionMsg(m.verify,
-                           `    source/unit_threaded/mock.d:123 - foo was called with unexpected Tuple!(int, string)(5, "foobar")` ~ "\n" ~
-                           `    source/unit_threaded/mock.d:123 -        instead of the expected Tuple!(int, string)(5, "quux")`);
-    }
-}
-
-///
-@("mock interface negative test")
-@safe pure unittest {
-    import unit_threaded.should;
-
-    interface Foo {
-        int foo(int, string) @safe pure;
-    }
-
     auto m = mock!Foo;
-    m.expect!"foo";
-    m.verify.shouldThrowWithMessage("Expected nth 0 call to foo did not happen");
-}
-
-// can't be in the unit test itself
-version(unittest)
-private class Class {
-    abstract int foo(int, string) @safe pure;
-    final int timesTwo(int i) @safe pure nothrow const { return i * 2; }
-    int timesThree(int i) @safe pure nothrow const { return i * 3; }
-    int timesThreeMutable(int i) @safe pure nothrow { return i * 3; }
-}
-
-@("mock class positive test")
-@safe pure unittest {
-
-    int fun(Class f) {
-        return 2 * f.foo(5, "foobar");
-    }
-
-    auto m = mock!Class;
-    m.expect!"foo";
+    m.expect!"foo"(5, "foobar");
     fun(m);
 }
 
-
-@("mock interface multiple calls")
-@safe pure unittest {
-    interface Foo {
-        int foo(int, string) @safe pure;
-        int bar(int) @safe pure;
-    }
-
-    void fun(Foo f) {
-        f.foo(3, "foo");
-        f.bar(5);
-        f.foo(4, "quux");
-    }
-
-    auto m = mock!Foo;
-    m.expect!"foo"(3, "foo");
-    m.expect!"bar"(5);
-    m.expect!"foo"(4, "quux");
-    fun(m);
-    m.verify;
-}
 
 ///
 @("interface expectCalled")
@@ -507,12 +433,11 @@ auto mockStruct(T...)(auto ref T returns) {
     return m;
 }
 
-// /**
-//    Version of mockStruct that accepts a compile-time mapping
-//    of function name to return values. Each template parameter
-//    must be a value of type `ReturnValues`
-//  */
-
+/**
+   Version of mockStruct that accepts a compile-time mapping
+   of function name to return values. Each template parameter
+   must be a value of type `ReturnValues`
+ */
 auto mockStruct(T...)() if(T.length > 0 && from!"std.meta".allSatisfy!(isReturnValue, T)) {
 
     struct Mock {
@@ -562,17 +487,6 @@ auto mockStruct(T...)() if(T.length > 0 && from!"std.meta".allSatisfy!(isReturnV
     m.verify;
 }
 
-///
-@("mock struct negative")
-@safe pure unittest {
-    import unit_threaded.asserts;
-
-    auto m = mockStruct;
-    m.expect!"foobar";
-    assertExceptionMsg(m.verify,
-                       "    source/unit_threaded/mock.d:123 - Expected nth 0 call to foobar did not happen\n");
-
-}
 
 ///
 @("mock struct values positive")
@@ -587,22 +501,6 @@ auto mockStruct(T...)() if(T.length > 0 && from!"std.meta".allSatisfy!(isReturnV
     m.verify;
 }
 
-///
-@("mock struct values negative")
-@safe pure unittest {
-    import unit_threaded.asserts;
-
-    void fun(T)(T t) {
-        t.foobar(2, "quux");
-    }
-
-    auto m = mockStruct;
-    m.expect!"foobar"(3, "quux");
-    fun(m);
-    assertExceptionMsg(m.verify,
-                       "    source/unit_threaded/mock.d:123 - foobar was called with unexpected Tuple!(int, string)(2, \"quux\")\n" ~
-                       "    source/unit_threaded/mock.d:123 -           instead of the expected Tuple!(int, string)(3, \"quux\")");
-}
 
 ///
 @("struct return value")
@@ -644,6 +542,7 @@ auto mockStruct(T...)() if(T.length > 0 && from!"std.meta".allSatisfy!(isReturnV
     m.expectCalled!"greet"("bar");
 }
 
+///
 @("mockStruct different return types for different functions and multiple return values")
 @safe pure unittest {
     import unit_threaded.should: shouldEqual;
@@ -661,41 +560,9 @@ auto mockStruct(T...)() if(T.length > 0 && from!"std.meta".allSatisfy!(isReturnV
 }
 
 
-@("const(ubyte)[] return type]")
-@safe pure unittest {
-    interface Interface {
-        const(ubyte)[] fun();
-    }
-
-    auto m = mock!Interface;
-}
-
-@("safe pure nothrow")
-@safe pure unittest {
-    interface Interface {
-        int twice(int i) @safe pure nothrow /*@nogc*/;
-    }
-    auto m = mock!Interface;
-}
-
-@("issue 63")
-@safe pure unittest {
-    import unit_threaded.should;
-
-    interface InterfaceWithOverloads {
-        int func(int) @safe pure;
-        int func(string) @safe pure;
-    }
-    alias ov = Identity!(__traits(allMembers, InterfaceWithOverloads)[0]);
-    auto m = mock!InterfaceWithOverloads;
-    m.returnValue!(0, "func")(3); // int overload
-    m.returnValue!(1, "func")(7); // string overload
-    m.expect!"func"("foo");
-    m.func("foo").shouldEqual(7);
-    m.verify;
-}
-
-
+/**
+   A mock struct that always throws.
+ */
 auto throwStruct(E = from!"unit_threaded.should".UnitTestException, R = void)() {
 
     struct Mock {
@@ -709,82 +576,11 @@ auto throwStruct(E = from!"unit_threaded.should".UnitTestException, R = void)() 
     return Mock();
 }
 
+///
 @("throwStruct default")
 @safe pure unittest {
     import unit_threaded.should: shouldThrow, UnitTestException;
     auto m = throwStruct;
     m.foo.shouldThrow!UnitTestException;
     m.bar(1, "foo").shouldThrow!UnitTestException;
-}
-
-version(testing_unit_threaded) {
-    class FooException: Exception {
-        import std.exception: basicExceptionCtors;
-        mixin basicExceptionCtors;
-    }
-
-
-    @("throwStruct custom")
-        @safe pure unittest {
-        import unit_threaded.should: shouldThrow;
-
-        auto m = throwStruct!FooException;
-        m.foo.shouldThrow!FooException;
-        m.bar(1, "foo").shouldThrow!FooException;
-    }
-}
-
-
-@("throwStruct return value type")
-@safe pure unittest {
-    import unit_threaded.asserts;
-    import unit_threaded.should: UnitTestException;
-    auto m = throwStruct!(UnitTestException, int);
-    int i;
-    assertExceptionMsg(i = m.foo,
-                       "    source/unit_threaded/mock.d:123 - foo was called");
-    assertExceptionMsg(i = m.bar,
-                       "    source/unit_threaded/mock.d:123 - bar was called");
-}
-
-@("issue 68")
-@safe pure unittest {
-    import unit_threaded.should;
-
-    int fun(Class f) {
-        // f.timesTwo is mocked to return 2, no matter what's passed in
-        return f.timesThreeMutable(2);
-    }
-
-    auto m = mock!Class;
-    m.expect!"timesThreeMutable"(2);
-    m.returnValue!("timesThreeMutable")(42);
-    fun(m).shouldEqual(42);
-}
-
-@("issue69")
-unittest {
-    import unit_threaded.should;
-
-    static interface InterfaceWithOverloadedFuncs {
-        string over();
-        string over(string str);
-    }
-
-    static class ClassWithOverloadedFuncs {
-        string over() { return "oops"; }
-        string over(string str) { return "oopsie"; }
-    }
-
-    auto iMock = mock!InterfaceWithOverloadedFuncs;
-    iMock.returnValue!(0, "over")("bar");
-    iMock.returnValue!(1, "over")("baz");
-    iMock.over.shouldEqual("bar");
-    iMock.over("zing").shouldEqual("baz");
-
-    auto cMock = mock!ClassWithOverloadedFuncs;
-    cMock.returnValue!(0, "over")("bar");
-    cMock.returnValue!(1, "over")("baz");
-    cMock.over.shouldEqual("bar");
-    cMock.over("zing").shouldEqual("baz");
 }
