efb47aa0b27206b251448836c1b1de28c71b9b43
Atila Neves <atila.neves@gmail.com>
2018-05-05 17:50:22 +0200

    Fix #96 - only build in unitThreadedLight mode

--- test.sh
+++ test.sh
@@ -5,5 +5,4 @@ set -euo pipefail
 DC="${DC:-dmd}"
 dub test --build=unittest-cov
 dub run -c unittest-unthreaded --build=unittest-cov --compiler="$DC"
-# See issue #96
-#dub run -c unittest-light --build=unittest
+dub build -c unittest-light --build=unittest
