d441987e85a8176463cc6ca57019b61dd329adf4
Atila Neves <atila.neves@gmail.com>
2018-08-15 21:36:07 +0200

    Run unit tests on appveyor

--- ci.bat
+++ ci.bat
@@ -1,6 +1,17 @@
 @echo off
 @setlocal
 
+rem Unit tests
+
+rem Regular tests
+dub test -q --build=unittest-cov
+rem Unthreaded tests
+dub run -q -c unittest-unthreaded --build=unittest-cov
+rem Light tests
+dub run -q -c unittest-light --build=unittest
+
+rem Integration tests
+
 pushd %~dp0\integration_tests\issue61
 dub run --build=unittest
 if %errorlevel% neq 0 exit /b %errorlevel%
