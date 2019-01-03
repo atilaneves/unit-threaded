c9704f4c3a662b3feb7fd3a9261d96ee8ef6df86
Atila Neves <atila.neves@gmail.com>
2019-01-03 16:47:10 +0100

    Quieten the integration test builds

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -7,19 +7,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 clear
 
 cd "$SCRIPT_DIR"/issue61
-dub run --build=unittest-cov
+dub run -q --build=unittest-cov
 
 cd "$SCRIPT_DIR"/issue109
-dub run --build=unittest-cov
+dub run -q --build=unittest-cov
 
 cd "$SCRIPT_DIR"/issue116
-dub run --build=unittest-cov
+dub run -q --build=unittest-cov
 
 cd "$SCRIPT_DIR"/runTestsMain
-dub run --build=unittest-cov
+dub run -q --build=unittest-cov
 
 cd "$SCRIPT_DIR"/issue121
-dub test && issue121_status=0 || issue121_status=1
+dub test -q && issue121_status=0 || issue121_status=1
 
 if [[ $issue121_status -eq 0 ]]; then
     echo "ERROR: issue121 should have failed but didn't"
