e2205db533a0d726e90a043c6e8dd163619749eb
Atila Neves <atila.neves@gmail.com>
2020-06-02 13:53:02 +0200

    Delete now useless test

--- tests/examples/pass/io.d
+++ tests/examples/pass/io.d
@@ -4,9 +4,5 @@ import unit_threaded;
 
 
 unittest {
-    writelnUt("Class writelnUt should only print with '-d' option");
-}
-
-unittest {
-    writelnUt("But this should show up when using -d option");
+    writelnUt("This should only be printed when using -d option");
 }
