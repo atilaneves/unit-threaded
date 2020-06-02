854593b7afc702add2706be2fd12b82df1cfd4c3
Atila Neves <atila.neves@gmail.com>
2020-06-02 14:40:23 +0200

    Fix integration tests

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -3,28 +3,29 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DC="${DC:-dmd}"
 
 clear
 
 cd "$SCRIPT_DIR"/issue61
 echo issue61
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue109
 echo issue109
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue116
 echo issue116
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/runTestsMain
 echo runTestsMain
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue121
 echo issue121
-dub test -q && issue121_status=0 || issue121_status=1
+dub test -q  --compiler="$DC" && issue121_status=0 || issue121_status=1
 
 
 if [[ $issue121_status -eq 0 ]]; then
@@ -37,8 +38,8 @@ fi
 
 cd "$SCRIPT_DIR"/issue157
 echo issue157
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue187
 echo issue187
-dub run -q --build=unittest-cov
+dub run -q --build=unittest-cov --compiler="$DC"
