02a1e1e190821f1dcdd3f111fe68f7257e8a1a55
Atila Neves <atila.neves@gmail.com>
2020-08-26 15:32:59 +0200

    Test for TERM in CI scripts

--- build/ut.sh
+++ build/ut.sh
@@ -4,23 +4,22 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DC="${DC:-dmd}"
-TERM="${TERM:-xterm}"
 
-clear
+[ -z "$TERM" ] || clear
 
 cd "$SCRIPT_DIR"/..
 
-printf 'Regular tests\n--------------------\n\n'
+[ -z "$TERM" ] || printf 'Regular tests\n--------------------\n\n'
 dub test -q --build=unittest-cov --compiler="$DC"
 
-printf '\n\nUnthreaded tests\n--------------------\n\n'
+[ -z "$TERM" ] || printf '\n\nUnthreaded tests\n--------------------\n\n'
 dub run -q -c unittest-unthreaded --build=unittest-cov --compiler="$DC"
 
-printf '\n\nLight tests\n--------------------\n\n'
+[ -z "$TERM" ] || printf '\n\nLight tests\n--------------------\n\n'
 dub run -q -c unittest-light --build=unittest --compiler="$DC"
 
 for dn in $(ls -d subpackages/*)
 do
-    printf '\n\n'$dn' tests\n--------------------\n\n'
+    [ -z "$TERM" ] || printf '\n\n'$dn' tests\n--------------------\n\n'
     dub test -q --compiler="$DC" --root="$dn"
 done
