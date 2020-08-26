519764e96e6e77c551ceedd731f957dc1544a072
Atila Neves <atila.neves@gmail.com>
2020-08-26 16:12:21 +0200

    Try TERM trick by MoonlightSentinel

--- build/ut.sh
+++ build/ut.sh
@@ -5,21 +5,21 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DC="${DC:-dmd}"
 
-[ -z "$TERM" ] || clear
+[ -z ${TERM-} ] || clear
 
 cd "$SCRIPT_DIR"/..
 
-[ -z "$TERM" ] || printf 'Regular tests\n--------------------\n\n'
+[ -z ${TERM-} ] || printf 'Regular tests\n--------------------\n\n'
 dub test -q --build=unittest-cov --compiler="$DC"
 
-[ -z "$TERM" ] || printf '\n\nUnthreaded tests\n--------------------\n\n'
+[ -z ${TERM-} ] || printf '\n\nUnthreaded tests\n--------------------\n\n'
 dub run -q -c unittest-unthreaded --build=unittest-cov --compiler="$DC"
 
-[ -z "$TERM" ] || printf '\n\nLight tests\n--------------------\n\n'
+[ -z ${TERM-} ] || printf '\n\nLight tests\n--------------------\n\n'
 dub run -q -c unittest-light --build=unittest --compiler="$DC"
 
 for dn in $(ls -d subpackages/*)
 do
-    [ -z "$TERM" ] || printf '\n\n'$dn' tests\n--------------------\n\n'
+    [ -z ${TERM-} ] || printf '\n\n'$dn' tests\n--------------------\n\n'
     dub test -q --compiler="$DC" --root="$dn"
 done
--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -6,50 +6,50 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DC="${DC:-dmd}"
 TERM="${TERM:-xterm}"
 
-[ -z "$TERM" ] || clear
+[ -z ${TERM-} ] || clear
 
 cd "$SCRIPT_DIR"/issue61
-[ -z "$TERM" ] || echo issue61
+[ -z ${TERM-} ] || echo issue61
 dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue109
-[ -z "$TERM" ] || echo issue109
+[ -z ${TERM-} ] || echo issue109
 dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/runTestsMain
-[ -z "$TERM" ] || echo runTestsMain
+[ -z ${TERM-} ] || echo runTestsMain
 dub run -q --build=unittest-cov --compiler="$DC"
 
 
 
 cd "$SCRIPT_DIR"/issue121
-[ -z "$TERM" ] || echo issue121
+[ -z ${TERM-} ] || echo issue121
 dub test -q  --compiler="$DC" && issue121_status=0 || issue121_status=1
 
 if [[ $issue121_status -eq 0 ]]; then
-    [ -z "$TERM" ] || echo "ERROR: issue121 should have failed but didn't"
+    [ -z ${TERM-} ] || echo "ERROR: issue121 should have failed but didn't"
     exit 1
 else
-    [ -z "$TERM" ] || printf "\\nDisregard the stack trace for issue121, it's supposed to fail\\n"
+    [ -z ${TERM-} ] || printf "\\nDisregard the stack trace for issue121, it's supposed to fail\\n"
 fi
 
 
 cd "$SCRIPT_DIR"/issue157
-[ -z "$TERM" ] || echo issue157
+[ -z ${TERM-} ] || echo issue157
 dub run -q --build=unittest-cov --compiler="$DC"
 
 cd "$SCRIPT_DIR"/issue187
-[ -z "$TERM" ] || echo issue187
+[ -z ${TERM-} ] || echo issue187
 dub run -q --build=unittest-cov --compiler="$DC"
 
 
 cd "$SCRIPT_DIR"/property-light
-[ -z "$TERM" ] || echo property-light
+[ -z ${TERM-} ] || echo property-light
 dub run -q && prop_light_status=0 || prop_light_status=1
 
 if [[ $prop_light_status -eq 0 ]]; then
-    [ -z "$TERM" ] || echo "ERROR: property-light should have failed but didn't"
+    [ -z ${TERM-} ] || echo "ERROR: property-light should have failed but didn't"
     exit 1
 else
-    [ -z "$TERM" ] || printf "\\nDisregard the stack trace for property-light, it's supposed to fail\\n"
+    [ -z ${TERM-} ] || printf "\\nDisregard the stack trace for property-light, it's supposed to fail\\n"
 fi
