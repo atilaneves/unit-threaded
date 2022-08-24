64b5713e526d2d34c0972523a2b8e234b882cc85
Atila Neves <atila.neves@gmail.com>
2022-08-24 16:49:34 -0300

    Update codecov config

--- build/ut.sh
+++ build/ut.sh
@@ -16,13 +16,13 @@ dub test -q --build=unittest-cov --compiler="$DC"
 dub run -q -c unittest-unthreaded --build=unittest-cov --compiler="$DC"
 
 [ -z ${TERM-} ] || printf '\n\nLight tests\n--------------------\n\n'
-dub run -q -c unittest-light --build=unittest --compiler="$DC"
+dub run -q -c unittest-light --build=unittest-cov --compiler="$DC"
 
 for dn in $(ls -d subpackages/*)
 do
     # the autorunner subpackage cannot be tested itself; see tests/integration_tests/autorunner
     if [[ "$dn" != "subpackages/autorunner" ]]; then
         [ -z ${TERM-} ] || printf '\n\n'$dn' tests\n--------------------\n\n'
-        dub test -q --compiler="$DC" --root="$dn"
+        dub test --build=unittest-cov -q --compiler="$DC" --root="$dn"
     fi
 done
